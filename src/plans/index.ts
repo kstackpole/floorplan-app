ab220554ace9ed2a4c0d97d5cb292830037264c2
Kyle Stackpole <ksta0821@Kyles-Work-Macbook.local>
2025-09-03 13:02:32 -0600

    PCO functionality enhanced, plan library updated

--- src/plans/index.ts
+++ src/plans/index.ts
@@ -1,6 +1,7 @@
 import type { SVGProps } from "../types/floorplan";
 import { planAOptions, PlanASVG } from "./planA";
 import { planBOptions, PlanBSVG } from "./planB";
+import { planCOptions, PlanCSVG } from "./planC";
 
 type Plan = {
   SVG: React.ComponentType<SVGProps>;
@@ -10,4 +11,5 @@ type Plan = {
 export const plans: Record<string, Plan> = {
   plana: { SVG: PlanASVG, options: planAOptions },
   planb: { SVG: PlanBSVG, options: planBOptions },
+  planc: { SVG: PlanCSVG, options: planCOptions },
 };
